8e448d4e53f36846a92677a06aab467cfcb1671d
Ocio75 <JonOcioGarcia@icloud.com>
2023-10-25 10:38:28 +0200

    Ventanas menu
    Se crean las ventanas de la tpv en blanco , el AdminSqlopnehelper

---
+++ AplicaionTPV/app/src/main/java/com/example/aplicaiontpv/menu_productos.java
@@ -0,0 +1,14 @@
+package com.example.aplicaiontpv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_productos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_productos);
+    }
+}
