78a906fcf367d9401a2d7df014f8c77ed630c4cc
Ocio75 <JonOcioGarcia@icloud.com>
2023-10-26 09:25:09 +0200

    Diseño loging y menu opciones

--- AplicaionTPV/app/src/main/java/com/example/aplicaiontpv/MainActivity.java
+++ AplicaionTPV/app/src/main/java/com/example/aplicaiontpv/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.aplicaiontpv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btLogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btLogIn=findViewById(R.id.btLogIn);
+    }
+    public void login(View v){
+        Intent i;
+        i = new Intent(this, Menu_opciones.class);
+        startActivity(i);
+        finish();
     }
 }
--- AplicaionTPV/app/src/main/java/com/example/aplicaiontpv/Menu_opciones.java
+++ AplicaionTPV/app/src/main/java/com/example/aplicaiontpv/Menu_opciones.java
@@ -2,13 +2,48 @@ package com.example.aplicaiontpv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Menu_opciones extends AppCompatActivity {
-
+    private Button btLogOut,btTerminar;
+    private ImageButton ibBebidas,ibMenu,ibCarta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_opciones);
+
+        btLogOut=findViewById(R.id.btLogout);
+        btTerminar=findViewById(R.id.btTerminar);
+        ibBebidas=findViewById(R.id.ibBebidas);
+        ibCarta=findViewById(R.id.ibCarta);
+        ibMenu=findViewById(R.id.ibMenu);
+    }
+    public void onClick(View v) {
+        Intent i;
+        if (v.equals(btLogOut)) {
+            i = new Intent(this, MainActivity.class);
+            startActivity(i);
+            finish();
+        } else if (v.equals(btTerminar)) {
+            i = new Intent(this, menu_terminar.class);
+            startActivity(i);
+            finish();
+        } else if (v.equals(ibBebidas)) {
+            i = new Intent(this, menu_productos.class);
+            startActivity(i);
+            finish();
+        }else if (v.equals(ibCarta)) {
+            i = new Intent(this, menu_productos.class);
+            startActivity(i);
+            finish();
+        } else if (v.equals(ibMenu)) {
+            i = new Intent(this, menu_menu.class);
+            startActivity(i);
+            finish();
+        }
     }
 }
