5826a038888435cc908eb7ddeef40e34b27cefff
Iriatv <133771760+Iriatv@users.noreply.github.com>
2023-10-26 11:02:50 +0200

    Diseño menu

--- AplicaionTPV/app/src/main/java/com/example/aplicaiontpv/menu_menu.java
+++ AplicaionTPV/app/src/main/java/com/example/aplicaiontpv/menu_menu.java
@@ -2,13 +2,39 @@ package com.example.aplicaiontpv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class menu_menu extends AppCompatActivity {
+
+    public RadioButton rbPrimero1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_menu);
+
+        rbPrimero1=findViewById(R.id.rbPrimero1);
+
+        rbPrimero1.setHeight(2);
+        rbPrimero1.setWidth(2);
     }
+
+    public void atras(View v){
+        Intent i;
+        i = new Intent(this, Menu_opciones.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void aceptar(View v){
+        Intent i;
+        i = new Intent(this, Menu_opciones.class);
+        startActivity(i);
+        finish();
+    }
+
 }
