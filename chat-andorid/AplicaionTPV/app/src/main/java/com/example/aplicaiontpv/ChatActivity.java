b0a39367be5208747f07c3738c20c310b6916cf0
Ocio75 <JonOcioGarcia@icloud.com>
2023-11-15 13:12:22 +0100

    Cambios de joritz
    Co-Authored-By: JoritzAres <147603322+JoritzAres@users.noreply.github.com>
    Co-Authored-By: Iriatv <133771760+Iriatv@users.noreply.github.com>

--- chat-andorid/AplicaionTPV/app/src/main/java/com/example/aplicaiontpv/ChatActivity.java
+++ chat-andorid/AplicaionTPV/app/src/main/java/com/example/aplicaiontpv/ChatActivity.java
@@ -1,25 +1,32 @@
 package com.example.aplicaiontpv;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class ChatActivity extends AppCompatActivity {
     private EditText messageEditText;
-    private TextView chatTextView;
+    private LinearLayout chatLayout;
     private Button sendButton;
 
     private Socket socket;
     private PrintWriter output;
+
+    private final Executor executor = Executors.newSingleThreadExecutor();
+    private String lastSentMessage = ""; // Almacena el último mensaje enviado
+    private String usuario; // Almacena el nombre de usuario
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,60 +34,95 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
 
         messageEditText = findViewById(R.id.messageEditText);
-        chatTextView = findViewById(R.id.chatTextView);
+        chatLayout = findViewById(R.id.chatLayout);
         sendButton = findViewById(R.id.sendButton);
 
-        new ClientAsyncTask().execute();
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sendMessage();
-            }
-        });
-    }
-
-    private void sendMessage() {
-        String message = messageEditText.getText().toString();
-        if (!message.isEmpty() && output != null) {
-            new SendMessageAsyncTask().execute(message);
-            messageEditText.setText("");
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.containsKey("USUARIO")) {
+            usuario = extras.getString("USUARIO");
+        } else {
+            usuario = "Usuario"; // Valor predeterminado si no se proporciona
         }
-    }
 
-    private class ClientAsyncTask extends AsyncTask<Void, String, Void> {
-        @Override
-        protected Void doInBackground(Void... voids) {
+        new Thread(() -> {
             try {
+                Log.d("ClientAsyncTask", "Intentando conectar al servidor...");
                 socket = new Socket("10.0.2.2", 12345);
+                Log.d("ClientAsyncTask", "Conectado al servidor.");
+
                 output = new PrintWriter(socket.getOutputStream(), true);
+
+                // Envía el nombre de usuario al servidor al establecer la conexión
+                output.println(usuario);
+
                 Scanner input = new Scanner(socket.getInputStream());
 
                 while (true) {
                     if (input.hasNext()) {
                         String message = input.nextLine();
-                        publishProgress(message);
+                        Log.d("ClientAsyncTask", "Mensaje recibido: " + message);
+
+                        // Verifica si el mensaje recibido es diferente al último mensaje enviado
+                        if (!message.equals(lastSentMessage)) {
+                            runOnUiThread(() -> {
+                                displayMessage(message, false);
+                            });
+                        }
                     }
                 }
             } catch (IOException e) {
+                Log.e("ClientAsyncTask", "Error de conexión: " + e.getMessage());
                 e.printStackTrace();
             }
-            return null;
-        }
+        }).start();
 
-        @Override
-        protected void onProgressUpdate(String... values) {
-            super.onProgressUpdate(values);
-            // Actualizar la interfaz de usuario con el mensaje recibido
-            chatTextView.append(values[0] + "\n");
+        sendButton.setOnClickListener(view -> {
+            sendMessage();
+        });
+    }
+
+    private void sendMessage() {
+        Log.d("ChatActivity", "Enviando mensaje...");
+        String message = messageEditText.getText().toString();
+        if (!message.isEmpty() && output != null) {
+            executor.execute(() -> {
+                try {
+                    Log.d("SendMessageAsyncTask", "Conectando al servidor");
+                    output.println( message);
+                    Log.d("SendMessageAsyncTask", "Mensaje enviado: " + message);
+
+                    // Actualiza el último mensaje enviado
+                    lastSentMessage = usuario + ": " + message;
+
+                    runOnUiThread(() -> {
+                        displayMessage(usuario + ": " + message, true);
+                    });
+                } catch (Exception e) {
+                    Log.e("SendMessageAsyncTask", "Error al enviar mensaje: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            });
+            messageEditText.setText("");
+            Log.d("ChatActivity", "Mensaje enviado correctamente.");
         }
     }
 
-    private class SendMessageAsyncTask extends AsyncTask<String, Void, Void> {
-        @Override
-        protected Void doInBackground(String... messages) {
-            output.println(messages[0]);
-            return null;
+    private void displayMessage(String message, boolean sentByMe) {
+        TextView textView = new TextView(this);
+        textView.setText(message);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        // Ajusta la gravedad según si el mensaje fue enviado por el propio cliente
+        if (sentByMe) {
+            layoutParams.gravity = Gravity.END;
+        } else {
+            layoutParams.gravity = Gravity.START;
         }
+
+        textView.setLayoutParams(layoutParams);
+        chatLayout.addView(textView);
     }
 }
--- chat-andorid/AplicaionTPV/app/src/main/java/com/example/aplicaiontpv/MainActivity.java
+++ chat-andorid/AplicaionTPV/app/src/main/java/com/example/aplicaiontpv/MainActivity.java
@@ -1,25 +1,32 @@
 package com.example.aplicaiontpv;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button btLogIn;
+    private EditText etUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btLogIn=findViewById(R.id.btLogIn);
+        btLogIn = findViewById(R.id.btLogIn);
+        etUsuario = findViewById(R.id.etUsuario);
     }
-    public void login(View v){
-        Intent i;
-        i = new Intent(this, ChatActivity.class);
+
+    public void login(View v) {
+        // Obtener el valor del campo etUsuario
+        String usuario = etUsuario.getText().toString();
+
+        // Pasar el valor a ChatActivity
+        Intent i = new Intent(this, ChatActivity.class);
+        i.putExtra("USUARIO", usuario); // Usamos "USUARIO" como clave para el valor
         startActivity(i);
         finish();
     }
